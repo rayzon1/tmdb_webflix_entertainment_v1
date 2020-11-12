ae8f117d5aa9537f85fd63c3130e3e58a5e9648c
Gerardo Keys <gerardokeys@Gerardos-MacBook-Pro.local>
2020-11-11 21:31:44 -0800

    update to config

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
-export const authToken = `${process.env.REACT_APP_AUTHTOKEN}`;
 export const apiKey = `${process.env.REACT_APP_APIKEY}`;
+export const authToken = `${process.env.REACT_APP_AUTHTOKEN}`;
 export const newsApiKey = `${process.env.REACT_APP_NEWS_APIKEY}`;
